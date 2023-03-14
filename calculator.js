e37e325ef5a2d59d9a6f866ce9f27c9de3a4840c
Filipa Merino <filipaam@outlook.com>
2023-03-14 13:42:07 +0000

    some sums

--- calculator.js
+++ calculator.js
@@ -1,7 +1,10 @@
 // jshint  esversion:6
 const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true }));
 
 app.get("/", function(req, res){
   // When we use .send() we are sending individual buts of HTML data;
@@ -11,7 +14,18 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res){
+  // var num1 = req.body.num1;
+  // var num2 = req.body.num2;
+  // var result = num1 + num2;  When we use body parser it turns everything into strings
+  // We need to specify that num1 & num2 are numbers;
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  result = num1 + num2;
+  
+  res.send("The result of the calculation is " + result);
+});
+
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
-
