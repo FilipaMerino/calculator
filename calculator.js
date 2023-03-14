2a40ef4c25f8621726cb46e970977a8b71107200
Filipa Merino <filipaam@outlook.com>
2023-03-14 14:02:48 +0000

    add bmi calculator

--- calculator.js
+++ calculator.js
@@ -14,15 +14,32 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+
+app.post("/bmicalculator", function(req, res){
+
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var n = weight / (height * height);
+
+  res.send("You BMI is " + n);
+});
+
+
+
 app.post("/", function(req, res){
   // var num1 = req.body.num1;
   // var num2 = req.body.num2;
   // var result = num1 + num2;  When we use body parser it turns everything into strings
   // We need to specify that num1 & num2 are numbers;
-  var num1 = Number(req.body.num1);
-  var num2 = Number(req.body.num2);
-  result = num1 + num2;
-  
+  var n1 = Number(req.body.n1);
+  var n2 = Number(req.body.n2);
+  var result = n1 + n2;
+
   res.send("The result of the calculation is " + result);
 });
 
