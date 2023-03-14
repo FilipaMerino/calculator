af2e4812b2eac5b2fc969560fefa9cab5a01610f
Filipa Merino <filipaam@outlook.com>
2023-03-14 13:16:29 +0000

    Sending entire files to routes

--- calculator.js
+++ calculator.js
@@ -4,9 +4,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Hello World!");
+  // When we use .send() we are sending individual buts of HTML data;
+  // There's a way for sending an entire file.
+  // res.sendFile(__dirname + "/ + nameOfFile")
+  // __dirname = file path of the current file no matter where is hosted
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
+
